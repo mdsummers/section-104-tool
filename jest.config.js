a5b8b391514981b76262ae2b2e6d4f79be99d58e
Matt Summers <devopsmatt@gmail.com>
2026-02-18 20:34:12 +0000

    Collect coverage from all relevant files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
   collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.js',
+    '!btc-cgt.js',
+    '!jest.config.js',
+    '!tests/**/*.js',
+    '!coverage/**/*.js',
+  ],
   coverageDirectory: 'coverage',
   verbose: true,
   testMatch: [
