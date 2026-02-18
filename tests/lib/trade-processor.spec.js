b9dc3ffbc27d427e43de3c319ee2a9ac9b9da747
Matt Summers <devopsmatt@gmail.com>
2026-02-18 13:19:14 +0000

    Add Example 3 HS284 from 2024

--- tests/lib/trade-processor.spec.js
+++ tests/lib/trade-processor.spec.js
@@ -130,8 +130,101 @@ describe('TradeProcessor', () => {
       expect(gain.eq('1650')).toBe(true); // (1750 - 100)
       expect(proceeds.eq('6000')).toBe(true);
       expect(qty.eq('4000')).toBe(true);
+    });
 
-      console.log(tp.process(trades));
+    const example3Trades = [{
+      id: '#1',
+      type: 'BUY',
+      date: new Date('2015-04-01T09:00:00.000Z'),
+      description: 'Bought 1000 shares in Lobster plc for 400p per share plus £150 fees',
+      qty: new Big('1000'),
+      total: new Big('4150'), // includes fee
+      fee: new Big('150'),
+      raw: {},
+    }, {
+      id: '#2',
+      type: 'BUY',
+      date: new Date('2018-09-01T09:00:00.000Z'),
+      description: 'Bought 500 shares in Lobster plc for 410p per share plus £80 fees',
+      qty: new Big('500'),
+      total: new Big('2130'), // includes fee
+      fee: new Big('80'),
+      raw: {},
+    }, {
+      id: '#3',
+      type: 'SELL',
+      date: new Date('2023-05-01T09:00:00.000Z'),
+      description: 'Sold 700 shares in Lobster plc for 480p per share (£3360) incurring £100 fees',
+      qty: new Big('700'),
+      total: new Big('3260'), // net of fee
+      fee: new Big('100'),
+      raw: {},
+    }, {
+      id: '#4',
+      type: 'SELL',
+      date: new Date('2024-02-01T09:00:00.000Z'),
+      description: 'Sold 400 shares in Lobster plc for 520p per share (£2080) incurring £105 fees',
+      qty: new Big('400'),
+      total: new Big('1975'), // net of fee
+      fee: new Big('105'),
+      raw: {},
+    }];
+    it('should follow example 3', () => {
+      const trades = example3Trades.slice(0, 3);
+      const tp = new TradeProcessor({
+        asset: 'Lobster plc',
+        currency: 'GBP',
+      });
+
+      const {
+        disposals,
+        pool: {
+          qty: poolQty,
+          cost: poolCost,
+        },
+      } = tp.process(trades);
+      expect(poolQty.eq('800')).toBe(true);
+      expect(poolCost.toFixed(0)).toBe('3349');
+
+      expect(disposals.length).toBe(1);
+      const [{
+        gain,
+        qty,
+        proceeds,
+      }] = disposals;
+      expect(gain.toFixed(0)).toBe('329');
+      expect(proceeds.toFixed(0)).toBe('3260');
+      expect(qty.toFixed(0)).toBe('700');
+    });
+
+    it('should follow example 3 - continued', () => {
+      const trades = example3Trades;
+      const tp = new TradeProcessor({
+        asset: 'Lobster plc',
+        currency: 'GBP',
+      });
+
+      const {
+        disposals,
+        pool: {
+          qty: poolQty,
+          cost: poolCost,
+        },
+      } = tp.process(trades);
+      expect(poolQty.eq('400')).toBe(true);
+      // the example rounds costs up and gains down
+      // we must round this down
+      expect(poolCost.toFixed(0, 0)).toBe('1674');
+
+      expect(disposals.length).toBe(2);
+      const [, {
+        gain,
+        qty,
+        proceeds,
+      }] = disposals;
+      expect(gain.toFixed(0)).toBe('300');
+      expect(proceeds.toFixed(0)).toBe('1975');
+      expect(qty.toFixed(0)).toBe('400');
     });
   }); // examples
 });
