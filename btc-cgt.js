9716a2d13b0babce715153006bdec708f481cd24
Matt Summers <devopsmatt@gmail.com>
2026-02-18 19:32:40 +0000

    InputFormat to provide asset and currency as fields

--- btc-cgt.js
+++ btc-cgt.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const InputFormat = require('./lib/input-format');
 const TradeProcessor = require('./lib/trade-processor');
-const { GBP } = require('./lib/currency');
-const { Bitcoin } = require('./lib/asset');
 
 // ===== CONFIG =====
 const FILE = './without-deposits.csv';
@@ -15,13 +13,17 @@ const raw = fs.readFileSync(FILE, 'utf8');
 
 // ===== INIT INPUT =====
 const input = InputFormat.from(raw);
+const {
+  asset,
+  currency,
+} = input;
 
 // ===== NORMALISE TRADES =====
 const trades = input.parseTrades();
 
 const tradeProcessor = new TradeProcessor({
-  asset: new Bitcoin(),
-  currency: GBP,
+  asset,
+  currency,
 });
 
 const {
@@ -34,10 +36,18 @@ const {
 // ===== OUTPUT =====
 console.log('Disposals:');
 disposals.forEach((d) => console.log(d.toString()));
-console.log('Total gain over timeframe:', GBP.format(gain));
+console.log('Total gain over timeframe:', currency.format(gain));
 
 console.log('\nSection 104 Pool:');
-console.log('BTC:', pool.qty.toFixed(8));
-console.log('Cost:', GBP.format(pool.cost));
-console.log('Fees:', GBP.format(fees.total));
-console.log('of which buy/sell: %s/%s', GBP.format(fees.buy), GBP.format(fees.sell));
+console.log(
+  '%s: %s',
+  asset.header(),
+  asset.formatAmountBare(pool.qty)
+);
+console.log('Cost:', currency.format(pool.cost));
+console.log('Fees:', currency.format(fees.total));
+console.log(
+  'of which buy/sell: %s/%s',
+  currency.format(fees.buy),
+  currency.format(fees.sell)
+);
